f6d4dc0ad2c1421a5d0b2f9ab4f632004804f304
rahulaes17 <rahulaes1704@gmail.com>
2026-02-27 14:16:33 +0530

    Deleting Todo

--- script.js
+++ script.js
@@ -5,44 +5,83 @@ let todoText;
 let todos = [];
 
 let todosString = localStorage.getItem("todos");
-if(todosString){
+if (todosString) {
     todos = JSON.parse(todosString);
 }
 
-const populateTodos = ()=>{
-    let string =""
+const populateTodos = () => {
+    let string = ""
     for (const todo of todos) {
-        string += `<li class="todo-item ${todo.isCompleted? "completed" : ""}">
-        <input type="checkbox" class="checkbox" ${todo.isCompleted? "checked" : ""}>
+        string += `<li id="${todo.id}" class="todo-item ${todo.isCompleted ? "completed" : ""}">
+        <input type="checkbox" class="checkbox" ${todo.isCompleted ? "checked" : ""}>
         <span class="text">${todo.title}</span>
         <button class="delete-btn">x</button>
     </li>`
     }
-    todoList.innerHTML = todoList.innerHTML + string;
+    todoList.innerHTML = string;
+
+
+    let deleteBtns = document.querySelectorAll(".delete-btn")
+
+    deleteBtns.forEach((element) => {
+        element.addEventListener("click", (dlt) => {
+            todos = todos.filter((todo) => {
+                return (todo.id) !== Number(dlt.target.parentNode.id)
+            })
+            localStorage.setItem("todos",JSON.stringify(todos))
+            populateTodos()
+        })
+    })
 }
 
 
 addTodoBtn.addEventListener("click", () => {
     todoText = inputTag.value;
+    if(todoText.trim().length<3){
+        alert("You cannot add a todo that small")
+        return
+    }
+    inputTag.value = "";
     let todo = {
-        title:todoText,
+        id: Date.now(),
+        title: todoText,
         isCompleted: false
     }
     todos.push(todo);
     localStorage.setItem("todos", JSON.stringify(todos));
-    inputTag.value = "";
     populateTodos();
 })
 populateTodos();
 
 const todoCheckboxes = document.querySelectorAll(".checkbox")
-todoCheckboxes.forEach(element => {
-    element.addEventListener("click",(tick) =>{
-        if(tick.target.checked){
+todoCheckboxes.forEach((element) => {
+    element.addEventListener("click", (tick) => {
+        if (tick.target.checked) {
             element.parentNode.classList.add("completed");
+            todos = todos.map(todo => {
+                if (todo.id == element.parentNode.id) {
+                    return { ...todo, isCompleted: true }
+                }
+                else {
+                    return todo;
+                }
+            })
+            localStorage.setItem("todos", JSON.stringify(todos))
         }
-        else{
+        else {
             element.parentNode.classList.remove("completed")
+
+            todos = todos.map(todo => {
+                if (todo.id == element.parentNode.id) {
+                    return { ...todo, isCompleted: false }
+                }
+                else {
+                    return todo;
+                }
+            })
+            localStorage.setItem("todos", JSON.stringify(todos))
         }
     })
 });
+
+
